6c7a65093a57c1523906e94b1a24350e5abcaded
Harshitha SC <HSC@iquartic.com>
2023-08-15 20:37:06 +0530

    Qualification page

--- index.js
+++ index.js
@@ -7,3 +7,28 @@ function topBarResponsive() {
         topBarId.className = "topBar";
     }
 }
+
+document.getElementById("workButton").style.backgroundColor = "#000000";
+document.getElementById("workButton").style.color = "#C7E9B0";
+
+function work() {
+    let education = document.getElementById("eduContent");
+    let work = document.getElementById("workContent");
+    education.style.display = "none";
+    work.style.display = "grid";
+    document.getElementById("workButton").style.backgroundColor = "#000000";
+    document.getElementById("workButton").style.color = "#C7E9B0";
+    document.getElementById("eduButton").style.color = "#000000";
+    document.getElementById("eduButton").style.backgroundColor = "#C7E9B0";
+}
+
+function education() {
+    let education = document.getElementById("eduContent");
+    let work = document.getElementById("workContent");
+    work.style.display = "none";
+    education.style.display = "grid";
+    document.getElementById("eduButton").style.backgroundColor = "#000000";
+    document.getElementById("eduButton").style.color = "#C7E9B0";
+    document.getElementById("workButton").style.color = "#000000";
+    document.getElementById("workButton").style.backgroundColor = "#C7E9B0";
+}
