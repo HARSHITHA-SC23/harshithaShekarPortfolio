4a82f36c989f09d4536b21a762155739084369f0
Harshitha SC <HSC@iquartic.com>
2023-08-16 17:19:36 +0530

    expertise page

--- index.js
+++ index.js
@@ -8,6 +8,8 @@ function topBarResponsive() {
     }
 }
 
+// Qualification JavaScript Code
+
 document.getElementById("workButton").style.backgroundColor = "#000000";
 document.getElementById("workButton").style.color = "#C7E9B0";
 
@@ -32,3 +34,23 @@ function education() {
     document.getElementById("workButton").style.color = "#000000";
     document.getElementById("workButton").style.backgroundColor = "#C7E9B0";
 }
+
+// Expertise JavaScript Code
+
+const buttonRight = document.getElementById("slideRight");
+const buttonLeft = document.getElementById("slideLeft");
+
+buttonRight.onclick = function () {
+  //   document.getElementById('skill-set').scrollLeft += 20;
+  document.getElementById("skill-set").scrollBy({
+    left: 250,
+    behavior: "smooth",
+  });
+};
+buttonLeft.onclick = function () {
+  //   document.getElementById('skill-set').scrollLeft -= 20;
+  document.getElementById("skill-set").scrollBy({
+    left: -250,
+    behavior: "smooth",
+  });
+};
